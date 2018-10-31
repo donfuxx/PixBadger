61c1dff8e7ada86808ad82ea65aaf94bf8e8c154
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2018-10-31 21:52:09 +0000

    copy misclassfied images folder structure

--- sh/copy_misclassified_img.sh
+++ sh/copy_misclassified_img.sh
@@ -7,7 +7,16 @@ echo "copy all misclassified images"
 input=${TF_TRAINING_PATH}/misclassified_test_images.txt
 while IFS= read -r var
 do
-  cp -v "../"$(echo "$var" | grep -oh '.*tf_files.*jpg') ${TF_TRAINING_PATH}"/misclassified_img"
+  
+  array=($var)
+  img_path=${array[0]}
+  err_label=${array[1]}
+  real_label=${array[2]}
+
+  dest_folder=${TF_TRAINING_PATH}"/misclassified_img/"${real_label}"/"${err_label}
+  mkdir -p ${dest_folder}
+  cp -v "../"${img_path} ${dest_folder}
+
 done < "$input"
 
-$SHELL
+#$SHELL
