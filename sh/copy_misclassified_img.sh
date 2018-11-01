bd1db53df2b31df3e709db9e76407019a6dc8a7d
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2018-11-01 23:04:54 +0000

    also delete bottlenecks of misclassified images

--- sh/copy_misclassified_img.sh
+++ sh/copy_misclassified_img.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 TF_TRAINING_PATH="../tf_files/training_reports"
+TF_BOTTLENECKS_PATH="../tf_files/bottlenecks"
 
 echo "copy all misclassified images"
 
@@ -10,15 +11,20 @@ do
   
   array=($var)
   img_path=${array[0]}
-  err_label=${array[1]}
-  real_label=${array[2]}
+  real_label=${array[1]}
+  err_label=${array[2]}
+  echo "${img_path} ${real_label} ${err_label}"
 
   dest_folder=${TF_TRAINING_PATH}"/misclassified_img/"${real_label}"/"${err_label}
   mkdir -p ${dest_folder}
   mv -v "../"${img_path} ${dest_folder}
+  
+  img_file=$(echo "${img_path}" | grep -oP '(?<=/)[^/]*\.jpg\b')
+  echo "delete bottleneck: ${real_label}/${img_file}"
+  find "${TF_BOTTLENECKS_PATH}/${real_label}" -name "${img_file}*" -delete
 
 done < "$input"
 
-rm ${input}
+rm -f "${input}"
 
 #$SHELL
