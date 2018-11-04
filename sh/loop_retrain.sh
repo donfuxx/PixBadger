512a7f1af57615fb42a85048be2c503f94240918
Thomas Fuchs-Martin <t.fuchsmartin@gmail.com>
2018-11-04 14:41:03 +0000

    loop retrain to train with different learning rates

--- sh/loop_retrain.sh
+++ sh/loop_retrain.sh
@@ -1,10 +1,38 @@
 #!/bin/bash
 
 BASE_DIR=$(dirname "$0")
+START=1
+DECR=1
+I=1
+LR=${START}
 
 while true
 do
-  ./retrain.sh
+
+  echo
+  echo "Learning Rate phase I = ${I}"
+
+  if [[ $I = 1 ]]
+    then LR=0.001
+    elif [[ $I = 2 ]]
+    then LR=0.0005
+    elif [[ $I = 3 ]]
+    then LR=0.0001
+    elif [[ $I = 4 ]]
+    then LR=0.00005
+    elif [[ $I = 5 ]]
+    then LR=0.00001
+    elif [[ $I = 6 ]]
+    then LR=0.000005
+    elif [[ $I = 7 ]]
+    then LR=0.000001
+    else $SHELL
+  fi
+
+  ((I++))
+
+  #LR=$(($LR+($I*$DECR)))
+  ./retrain.sh "${LR}"
   cd ${BASE_DIR}
   ./copy_misclassified_img.sh
 done
--- sh/retrain.sh
+++ sh/retrain.sh
@@ -3,9 +3,10 @@
 TF_PATH=".."
 IMAGE_SIZE=224
 ARCHITECTURE="mobilenet_1.0_${IMAGE_SIZE}"
-STEPS=1000
-LEARNING_RATE=0.0001
-TRAIN_SIZE=100
+STEPS=100000
+#LEARNING_RATE=0.00001
+LEARNING_RATE=$1
+TRAIN_SIZE=32
 
 source ~/venv/bin/activate
 
@@ -22,14 +23,16 @@ python -um scripts.retrain \
   --how_many_training_steps=${STEPS} \
   --learning_rate=${LEARNING_RATE} \
   --train_batch_size=${TRAIN_SIZE} \
-  --validation_batch_size=-1 \
+  --validation_batch_size=128 \
   --model_dir=tf_files/models/ \
-  --summaries_dir=tf_files/training_summaries/"${ARCHITECTURE}"/LR_"${LEARNING_RATE}"/TS_"${TRAIN_SIZE}"/MV_4 \
+  --summaries_dir=tf_files/training_summaries/"${ARCHITECTURE}"/LR_"${LEARNING_RATE}"/TS_"${TRAIN_SIZE}"/MV_L1 \
   --output_graph=tf_files/retrained_graph.pb \
   --output_labels=tf_files/retrained_labels.txt \
   --architecture="${ARCHITECTURE}" \
   --image_dir=tf_files/flower_photos \
   --print_misclassified_test_images
+
+sleep 1
 
 #$SHELL
 
